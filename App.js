518c338822209ee2d44a214899eb0f7e157a20b0
John Martin <curmudgeon@rallydev.com>
2013-04-04 13:50:57 -0700

    First cut.

---
+++ App.js
@@ -0,0 +1,92 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    layout: { type: 'hbox' },
+    defaults: { padding: 10 },
+    items: [
+        {xtype: 'container',itemId:'render_example', flex: 1},
+        {xtype: 'container',itemId:'line_by_line_example', flex: 1}
+    ],
+    launch: function() {
+        this._getSomeStories();
+    },
+    _getSomeStories: function() {
+        if ( this.render_grid ) { this.render_grid.destroy(); }
+        if ( this.line_grid ) { this.line_grid.destroy(); }
+        Ext.create('Rally.data.WsapiDataStore',{
+            autoLoad: true,
+            model: 'User Story',
+            fetch: ['RevisionHistory','Revisions','FormattedID','Name','RevisionNumber','CreationDate','User'],
+            listeners: {
+                load: function(store,data,success) {
+                    this._showRenderedGrid(store);
+                    this._prepareLineByLineGrid(data);
+                },
+                scope: this
+            }
+        });
+    },
+    /* formats data for the revision history cell */
+    _revisionRenderer: function(value) {
+        var formatted_value_array = [];
+        if ( value.Revisions ) {
+            Ext.Array.each( value.Revisions, function(rev){
+                    formatted_value_array.push( rev.RevisionNumber + " on " + rev.CreationDate + " by " + rev.User._refObjectName );
+            });
+        }
+        return formatted_value_array.join("<br/>");
+    },
+    _showRenderedGrid: function(store) {
+        if ( this.render_grid ) { this.render_grid.destroy(); }
+        this.render_grid = Ext.create('Rally.ui.grid.Grid',{
+            store: store,
+            columnCfgs: [
+                { text: 'ID', dataIndex: 'FormattedID' },
+                { text: 'Name', dataIndex: 'Name' },
+                { text: 'Revs', dataIndex: 'RevisionHistory', renderer: this._revisionRenderer, flex: 1}
+            ]
+        });
+        this.down('#render_example').add(this.render_grid);
+    },
+    _prepareLineByLineGrid: function(data) {
+        var lines = [];
+        /* TODO: see if this is what a TransformStore might be good for? */
+        Ext.Array.each(data,function(item) {
+            Ext.Array.each(item.get('RevisionHistory').Revisions, function(rev){
+                var line = {
+                    FormattedID: item.get('FormattedID'),
+                    Name: item.get('Name'),
+                    RevisionNumber: rev.RevisionNumber,
+                    RevisionDate: rev.CreationDate,
+                    RevisionAuthor: rev.User._refObjectName
+                };
+                lines.push(line);
+            });
+        });
+        /* could be much more compact, but I like to separate the data gathering from the data display */
+        Ext.create('Rally.data.custom.Store',{
+            autoLoad: true,
+            data: lines,
+            listeners: {
+                load: function(store,data,success) {
+                    this._showLineByLineGrid(store);
+                },
+                scope: this
+            }
+        });
+    },
+    _showLineByLineGrid: function(store) {
+        if ( this.line_grid ) { this.line_grid.destroy(); }
+        this.line_grid = Ext.create('Rally.ui.grid.Grid',{
+            store: store,
+            columnCfgs: [
+                { text: 'ID', dataIndex: 'FormattedID' },
+                { text: 'Name', dataIndex: 'Name' },
+                { text: 'id', dataIndex: 'RevisionNumber'},
+                { text: 'date', dataIndex: 'RevisionDate', flex: 1 },
+                { text: 'author', dataIndex: 'RevisionAuthor' }
+            ]
+        });
+        this.down('#line_by_line_example').add(this.line_grid);
+    }
+});
